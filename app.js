f58d4d1ac7f70a4821cef4097f00955c9aac7eb1
Druzzdil <staniszewski86@gmail.com>
2017-06-06 16:46:14 +0200

    second

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 /**
  * Created by igor on 6/6/2017.
  */
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'hello Igor', function (err) {
-    if (err) {
-        console.log('smt went wrong dude');
-    }
+let res = notes.addNote();
+let add = notes.add(3,6);
+console.log('result:', notes.add(3,5));
+let user = os.userInfo();
+
+fs.appendFile('greetings.txt', `hello ${user.username}!` + add , res => {
+    if (err) throw error;
 });
---
+++ notes.js
@@ -0,0 +1,13 @@
+/**
+ * Created by igor on 6/6/2017.
+ */
+console.log('starting notes.js');
+
+module.exports.addNote =  () => {
+    console.log('new note');
+    return 'new noteee'
+}
+module.exports.add = (a,b) => {
+    return a+b;
+}
+
